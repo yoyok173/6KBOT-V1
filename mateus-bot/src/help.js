331b543b4b2ab8e8cc8ddbc5e627268259ef928a
styxxdev <pgsouza48@gmail.com>
2021-03-13 14:07:25 -0300

    6KV1

---
+++ mateus-bot/src/help.js
@@ -0,0 +1,32 @@
+const help = (prefix) => {
+	return `
+
+  ◁━━━━━━━━◈✙◈━━━━━━━━▷
+
+  𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦:
+
+  ► ${prefix}marcar
+  ► ${prefix}marcar2
+  ► ${prefix}promover
+  ► ${prefix}add
+  ► ${prefix}rebaixar
+  ► ${prefix}banir
+
+   👨‍💻 *Programador:* wa.me/5516993079236 Styxx
+
+  ◁━━━━━━━━◈✙◈━━━━━━━━▷
+`
+}
+
+exports.help = help
+
+
+
+
+
+
+
+
+
+
+
